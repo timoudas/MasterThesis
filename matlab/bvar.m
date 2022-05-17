9ea5a84b787c3dbbdc0e44edacf82f6b9821282e
Andreas Timoudas <andreas.timoudas@fcgfonder.se>
2022-05-17 16:35:37 +0200

    changes

--- matlab/bvar.m
+++ matlab/bvar.m
@@ -56,11 +56,13 @@ s.favar.FAVAR=0;
 % 31=Independent Normal-Wishart(S0 as univariate AR), 32=Independent Normal-Wishart(S0 as identity)
 % 41=Normal-diffuse
 % 51=Dummy observations
-s.prior=12;
-% hyperparameter: autoregressive coefficient
-s.ar=0.8;
+% 61=Steady State priors
+s.prior=61;
+% hyperparameter: autoregressive coefficient (if PriorExcel is equal to 0)
+% switch to Excel interface
+s.ar=1;
 % set to 1 if you want individual priors, 0 for default
-s.PriorExcel=0; 
+s.PriorExcel=1; 
 % set to 1 if you want individual priors, 0 for default
 s.priorsexogenous=0; 
 % hyperparameter: lambda1
@@ -82,7 +84,7 @@ s.lambda8=1;
 
 
 % total number of iterations for the Gibbs sampler
-s.It=5000;
+s.It=10000;
 % number of burn-in iterations for the Gibbs sampler
 s.Bu=2000;
 % hyperparameter optimisation by grid search (1=yes, 0=no)
@@ -97,14 +99,13 @@ s.iobs=0;
 s.lrp=0;
 % create H matrix for the long run priors % H=[1 1 0 0;-1 1 0 0;0 0 1 1;0 0 -1 1];
 % now taken from excel loadH.m
-
 s.priorf=100;
 
 %% Model options
 s.IRF=1;          % activate impulse response functions (1=yes, 0=no)
 s.IRFperiods=20;  % number of periods for impulse response functions
-s.F=1;            % activate unconditional forecasts (1=yes, 0=no)
-s.FEVD=0;         % activate forecast error variance decomposition (1=yes, 0=no)
+s.F=0;            % activate unconditional forecasts (1=yes, 0=no)
+s.FEVD=1;         % activate forecast error variance decomposition (1=yes, 0=no)
 s.HD=1;           % activate historical decomposition (1=yes, 0=no)
 s.HDall=0;        % if we want to plot the entire decomposition, all contributions (includes deterministic part)HDall
 s.CF=0;           % activate conditional forecasts (1=yes, 0=no)
@@ -113,21 +114,24 @@ s.CFt=1;          % 1=standard (all shocks), 2=standard (shock-specific), 3=tilt
 % structural identification (1=none, 2=Cholesky, 3=triangular factorisation, 4=sign, zero, magnitude, relative magnitude, FEVD, correlation restrictions,
 % 5=IV identification, 6=IV identification & sign, zero, magnitude, relative magnitude, FEVD, correlation restrictions)
 s.IRFt=2;
+% Correlation restriction options:
+%s.strctident.CorrelShock=''; % exact labelname of the shock defined in one of the "...res values" excel sheets, otherwise if the shock is not identified yet name it 'CorrelShock'
+%s.strctident.CorrelInstrument=''; % provide the IV variable in excel sheet "IV"
 
 %% Forecast 
-s.Fstartdate='2019q1'; % start date for forecasts (has to be an in-sample date; otherwise, ignore and set Fendsmpl=1)
-s.Fenddate='2021q4';   % end date for forecasts
-s.Fendsmpl=1;          % start forecasts immediately after the final sample period (1=yes, 0=no) has to be set to 1 if start date for forecasts is not in-sample
+s.Fstartdate='2012q1'; % start date for forecasts (has to be an in-sample date; otherwise, ignore and set Fendsmpl=1)
+s.Fenddate='2021q3';   % end date for forecasts
+s.Fendsmpl=0;          % start forecasts immediately after the final sample period (1=yes, 0=no) has to be set to 1 if start date for forecasts is not in-sample
 s.Feval=0;             % activate forecast evaluation (1=yes, 0=no)
 s.hstep=1;             % step ahead evaluation
 s.window_size=0;       % window_size for iterative forecasting 0 if no iterative forecasting
 s.evaluation_size=0.5; % evaluation_size as percent of window_size                                      <                                                                                    -
 
 %% Credibility bands
-s.cband=0.68;    % confidence/credibility level for VAR coefficients
-s.IRFband=0.68;  % confidence/credibility level for impusle response functions
+s.cband=0.95;    % confidence/credibility level for VAR coefficients
+s.IRFband=0.95;  % confidence/credibility level for impusle response functions
 s.Fband=0.68;    % confidence/credibility level for forecasts
-s.FEVDband=0.68; % confidence/credibility level for forecast error variance decomposition
+s.FEVDband=0.95; % confidence/credibility level for forecast error variance decomposition
 s.HDband=0.68;   % confidence/credibility level for historical decomposition
 
 BEARmain(s)
--- matlab/code.m
+++ matlab/code.m
@@ -13,10 +13,12 @@ vnames = char('SFSI', 'BNP', 'CPI', 'DINT');
 
 %[icVar, infocrit, tresh] = tvarxic(y,10,1,th, options_ic, options_tvarx);
 
-result = tvarx(y, 3, 1, th, 2, struct('prtr', true, 'optimcrit', 'logdet', 'const', true));
+result = tvarx(y, 1, 1, th, 1, struct('prtr', true, 'optimcrit', 'logdet', 'const', true));
 
-irf_options = struct('sqrttype', 'chol', 'vnames', vnames, 'graph', true); 
+irf_options = struct('sqrttype', 'girf', 'vnames', vnames, 'graph', true); 
 %[IRF,bootIRFLowerCI,bootIRFUpperCI,bootIRFMedCI,bootIRFAll,impact] = tvarxlirf(result, irf_options);
 
+
+disp(result)
 
 
