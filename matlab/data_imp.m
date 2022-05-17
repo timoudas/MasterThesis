d1d8d7ccfe24a58c8e90f037db878865b30f6b56
Andreas Timoudas <andreas.timoudas@fcgfonder.se>
2022-05-17 16:35:58 +0200

    c

--- matlab/data_imp.m
+++
@@ -1,8 +0,0 @@
-[data,names]=xlsread('datab.xls');
-
-datestrings=names(2:end,1);
-startdate = '1995q4';
-enddate = '2021q3';
-startlocationData=find(strcmp(datestrings,startdate));
-endlocation=find(strcmp(datestrings,enddate));
-data=data(startlocationData:endlocation);
